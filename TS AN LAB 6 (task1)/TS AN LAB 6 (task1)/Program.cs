bf44a36ead4d915768fd2e406c7236b6c15ccd27
Morkkkovka <aaa988248@gmail.com>
2021-11-22 20:02:11 +0200

    Lab 6

---
+++ TS AN LAB 6 (task1)/TS AN LAB 6 (task1)/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_AN_LAB_6__task1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class AbstractHandler
+    {
+        public void Handle(Document document)
+        {
+            string[] lines = document.Read();
+
+            for (int i = lines.Length - 1; i >= 0; i--)
+                document.Write(lines[i]);
+        }
+    }
+
+
+    public abstract class Document
+    {
+        public abstract string Name { get; protected set; }
+
+        public abstract void Open(string path);
+        public abstract void Create(string path);
+        public abstract string[] Read();
+        public abstract void Write(string line);
+    }
+    public class RTF : Document
+    {
+        public override string Name { get; protected set; }
+
+        public override void Create(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Open(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DOC : Document
+    {
+        public override string Name { get; protected set; }
+
+        public override void Create(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Open(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class ODT  : Document
+    {
+        public override string Name { get; protected set; }
+
+        public override void Create(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Open(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class PDF : Document
+    {
+        public override string Name { get; protected set; }
+
+        public override void Create(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Open(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+   
+}
---
+++ TS AN LAB6 (task3)/TS AN LAB6 (task3)/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_AN_LAB6__task3_
+{
+    interface IHasInfo
+    {
+        void ShowInfo();
+    }
+    interface IServices 
+    {
+        void ForUser();
+        int Cost { get; }
+
+    }
+
+    abstract class Services : IHasInfo, IServices
+    {
+        
+        public abstract int Cost { get; }
+
+        public abstract void ForUser();
+        
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{GetType().Name} Цена: {Cost}");
+        }
+    }
+
+    class Sale : Services
+    {
+        public override int Cost => 1950;
+        public override void ForUser()
+        {
+            Console.WriteLine("Эта услуга дает возможность продажи нужной информации!");
+        }
+
+    }
+    class Consultution : Services
+    {
+        public override int Cost { get { return 1020; } }
+        public override void ForUser()
+        {
+            Console.WriteLine("Эта услуга дает возможность проконсультироватся!");
+        }
+
+    }
+
+    class Buing : Services
+    {
+        public override int Cost => 3065;
+
+        public override void ForUser()
+        {
+            Console.WriteLine("Эта услуга дает возможность покупки!");
+        }
+    }
+
+    class Company : IHasInfo
+    {
+        public void ShowInfo()
+        {
+            Console.WriteLine("Больше услуг нет!");
+        }
+    }
+    class User
+    {
+        public void ForUser(IServices services)
+        {
+            services.ForUser();
+        }
+        public void CheckInfo(IHasInfo hasInfo)
+        {
+            hasInfo.ShowInfo();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            Services[] ser = { new Sale(), new Consultution(), new Buing() };
+            foreach (var choice in ser)
+            {
+                user.CheckInfo(choice);
+                user.ForUser(choice);
+                Console.WriteLine();
+            }
+            user.CheckInfo(new Company());
+        }
+    }
+}
